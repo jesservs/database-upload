7e1a74ae6d20011ed4b50df0a0b502363195e007
jesservs <jesservs@hotmail.com>
2020-09-16 13:51:31 -0300

    feature: Input file functionality created

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+
+// const CPUs = require('os').cpus().length;
+
+interface TransactionDTO {
+  title:string;
+  value:number;
+  type:'income'|'outcome';
+}
 
 interface Balance {
   income: number;
@@ -11,7 +20,35 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch(transaction.type){
+          case "income":
+            accumulator.income += Number(transaction.value);
+            break;
+
+          case "outcome":
+            accumulator.outcome += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income : 0,
+        outcome: 0,
+        total: 0
+      }
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
